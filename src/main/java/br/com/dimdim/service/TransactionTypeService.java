c71628a6ad559d8a504798c958b85a5de4215d45
Nathan <Nathansilva18@outlook.com>
2022-08-23 22:35:12 -0300

    Criação entity, typeService, TypeRepository

--- src/main/java/br/com/dimdim/service/TransactionTypeService.java
+++ src/main/java/br/com/dimdim/service/TransactionTypeService.java
@@ -1,4 +1,48 @@
 package br.com.dimdim.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.dimdim.entity.TransactionType;
+import br.com.dimdim.repository.TransactionTypeRepository;
+
+@Service
 public class TransactionTypeService {
+    @Autowired
+    private TransactionTypeRepository transactionTypeRepository;
+
+    public List<TransactionType> listAllTransactions(){return  transactionTypeRepository.findAll();}
+
+    public TransactionType getTransactionById(Integer id) throws Exception {
+        return transactionTypeRepository.findById(id).orElseThrow(() -> new Exception("Transaction not found"));
+    }
+
+    public void updateTransaction(Integer id, TransactionType transactionType) throws Exception {
+        if (transactionTypeRepository.findById(id).isEmpty())
+            throw new Exception("Transaction not found");
+        transactionType.setId(id);
+        transactionTypeRepository.save(transactionType);
+    }
+
+    public void createTransaction(TransactionType transactionType) throws Exception {
+        if (transactionType == null)
+            throw new NullPointerException("Null object");
+
+        if (transactionType.getType() == null)
+            throw new Exception("Not accept null values");
+
+        transactionTypeRepository.save(transactionType);
+    }
+
+    public void deleteTransaction(Integer id) throws Exception {
+        if (transactionTypeRepository.findById(id).isEmpty())
+            throw new Exception("Transaction not found");
+
+        transactionTypeRepository.deleteById(id);
+    }
+
+
+
 }
